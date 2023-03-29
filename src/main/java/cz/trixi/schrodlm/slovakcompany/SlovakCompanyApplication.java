7b0265d7328c3bb784f5d309b22af323b93d870d
schrodlm <msamot@seznam.cz>
2023-03-29 15:38:48 +0200

    Initial commit

---
+++ src/main/java/cz/trixi/schrodlm/slovakcompany/SlovakCompanyApplication.java
@@ -0,0 +1,13 @@
+package cz.trixi.schrodlm.slovakcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlovakCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlovakCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/trixi/schrodlm/slovakcompany/SlovakCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.trixi.schrodlm.slovakcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlovakCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
